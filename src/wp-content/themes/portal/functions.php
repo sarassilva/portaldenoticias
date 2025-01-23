d9d2a64db218d81c47917c86c0e1ca710e1acb52
Sara Silva <sarassilvadw@gmail.com>
2025-01-23 13:06:26 -0300

    Requests

--- src/wp-content/themes/portal/functions.php
+++ src/wp-content/themes/portal/functions.php
@@ -12,3 +12,17 @@ add_action('wp_enqueue_scripts', 'react_theme_scripts');
 
 add_theme_support( 'post-thumbnails' );
 
+header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline'; connect-src 'self' http://localhost:8080");
+
+function allow_cors() {
+    header("Access-Control-Allow-Origin: http://localhost:3000"); // Ajuste para o domínio correto
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    
+    // Para a requisição OPTIONS (pré-vôo)
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        exit(0);
+    }
+}
+
+add_action('init', 'allow_cors');
